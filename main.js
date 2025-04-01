33b889ac314376b02fb326b44d361f26c2ce1e83
davideerusso <rd.96.rd@gmail.com>
2025-04-01 18:04:13 +0200

    correction

--- main.js
+++ main.js
@@ -2,33 +2,34 @@ const nameUser = prompt("Come ti chiami?", "Davide");
 const ageUser = parseInt(prompt("Quanti anni hai?", "29"));
 const kmUser = parseInt(prompt("Quanti Km vuoi percorrere?", "10"));
 
-let Pricekm = kmUser * 0.21;
-let discount20 = (Pricekm * 20) / 100;
-let discount40 = (Pricekm * 40) / 100;
-let Pricediscount20 = Pricekm - discount20;
-let Pricediscount40 = Pricekm - discount40;
+const pricekm = kmUser * 0.21; // pricekm -> String
+
+const discount20 = (pricekm * 20) / 100;
+const discount40 = (pricekm * 40) / 100;
+const pricediscount20 = pricekm - discount20;
+const pricediscount40 = pricekm - discount40;
 
 console.log(nameUser);
 console.log(ageUser);
 console.log(kmUser);
-console.log(Pricekm);
-console.log(Pricediscount20);
-console.log(Pricediscount40);
+console.log(pricekm);
+console.log(pricediscount20);
+console.log(pricediscount40);
 
 let outputText;
 
 //controllo validità input
 
-if (nameUser.length > 0 && !isNaN(ageUser) && ageUser > 0) {
+if (nameUser.length > 0 && !isNaN(ageUser) && ageUser > 0 && kmUser > 0) {
   //se l'età è inferiore a 18
   if (ageUser < 18) {
-    outputText = "Il Prezzo è di" + " " + Pricediscount20 + "€";
+    outputText = "Il Prezzo è di" + " " + pricediscount20.toFixed(2) + "€";
   }
   //se l'età è superiore a 65
-  if (ageUser > 65) {
-    outputText = "Il Prezzo è di" + " " + Pricediscount40 + "€";
+  else if (ageUser > 65) {
+    outputText = "Il Prezzo è di" + " " + pricediscount40.toFixed(2) + "€";
   } else {
-    outputText = "Il Prezzo è di" + " " + Pricekm + "€";
+    outputText = "Il Prezzo è di" + " " + pricekm + "€";
   }
 }
 
